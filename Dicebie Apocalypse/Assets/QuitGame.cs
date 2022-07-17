a7987e2855b478245a4a03a7c9c824b6b244795e
Space-c2 <50313983+Space-c2@users.noreply.github.com>
2022-07-17 18:31:11 +0700

    stuff
    random stuff

--- Dicebie Apocalypse/Assets/QuitGame.cs
+++ Dicebie Apocalypse/Assets/QuitGame.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class QuitGame : MonoBehaviour
 {
+    public int count;
+    public GameObject Quitting;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,20 @@ public class QuitGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.Escape))
+        {
+            count = count + 1;
+            Quitting.SetActive(true);
+        }
+        else
+        {
+            count = 1;
+            Quitting.SetActive(false);
+        }
+        if(count > 600)
+        {
+            Debug.Log("QuitGame");
+            SceneManager.LoadScene(0);
+        }
     }
 }
